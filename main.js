c232a802b7940ed88a8236f9b75b5b8930581b33
Rehan Gillani <rehangillani.edu@gmail.com>
2018-09-26 08:56:41 +0500

    This is template app layout

---
+++ main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    if($(window).height() < 560) {
+       $("span").removeClass("fa-2x"); 
+
+    } 
+    if($(window).height() == 568) {
+       $("span").removeClass("logo-cent-small"); 
+
+    } 
+    if($(window).height() == ) {
+       $("span").removeClass("logo-cent"); 
+
+    } 
+});
